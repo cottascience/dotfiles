8b00b54ee202cc01828757dc8d9e5b96da5a2177
Leo Cotta <leoabreucotta@gmail.com>
2026-03-28 08:21:33 +0000

    treescope cleaning

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,10 +1,5 @@
 -- Todo-comments: Highlight and search for todo comments
--- LazyVim provides todo-comments, this adds your keymaps
+-- Snacks picker extra maps <leader>st and <leader>sT
 return {
   "folke/todo-comments.nvim",
-  keys = {
-    -- Your custom keymaps (LazyVim uses <leader>st, <leader>sT, <leader>xt, <leader>xT)
-    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find Todo Comments" },
-    { "<leader>fT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Find Todo/Fix/Fixme" },
-  },
 }
